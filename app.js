440fc9e6e414c8555b9a4ed52b2a1b5b01a24d99
Shu Min Yang Chang <shu.minyang@hotmail.com>
2017-05-22 00:21:15 -0300

    commit

--- app.js
+++ app.js
@@ -5,27 +5,13 @@ var app = angular.module('MyInvest', []);
 var controllerForm = app.controller('FormController', function () {
     var vm = this;
 
-    vm.inputName = '';
     vm.inputEmail = '';
-    vm.motivo = '';
     vm.emailError = false;
-
-    vm.enviarServidor = function () {
-        validarCampos();
-    };
 
     vm.verificarEmail = function () {
         if (this.inputEmail !== '') {
             vm.emailError = validarEmail();
-        }
-    };
-
-    var validarCampos = function () {
-        if (vm.inputName !== '' && vm.inputEmail !== '') {
-            console.log('enviar');
-        } else {
-            console.log('conte erros');
-        }
+        }       
     };
 
     var validarEmail = function () {
@@ -37,23 +23,4 @@ var controllerForm = app.controller('FormController', function () {
 
         return false;
     };
-});
-
-var controllerBody = app.controller('BodyController', function () {
-
-    var vm = this;
-
-    vm.textoEstrutura = true;
-    vm.pesquisa = false;
-
-    vm.mostrarPesquisa = function () {
-        vm.textoEstrutura = false;
-        vm.pesquisa = true;
-    };
-
-    vm.mostrarTexto = function () {
-        vm.textoEstrutura = true;
-        vm.pesquisa = false;
-    }
-
 });
